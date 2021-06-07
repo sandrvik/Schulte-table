f52ef7c2b212a098b818bd3a8ee7a708a53dc82b
dybovik <sandrvik@gmail.com>
2021-06-07 12:11:57 +0300

    structure changes

--- schulte.js
+++ schulte.js
@@ -45,51 +45,50 @@ function createArr(s) {
     return arr;
 }
 
-
-document.querySelector('.startButton').addEventListener('click', function () {
-    createTable();
-    document.querySelector('.mainContainer').classList.remove('hidden');
-    document.querySelector('.initialContainer').classList.add('hidden');
-    startTime = new Date().getTime();
-})
-
 // Style and create the table
 function createTable() {
     let container = document.querySelector('.container');
     for (let i = 0; i < Math.pow(size, 2); i++) {
-        let button = document.createElement('button');
-        button.classList.add('button', 'wrongButton');
-        button.style.width = 98 / size + 'vmin';
-        button.style.height = 98 / size + 'vmin';
-        container.append(button);
+        let cell = document.createElement('button');
+        cell.classList.add('cell');
+        cell.style.width = 98 / size + 'vmin';
+        cell.style.height = 98 / size + 'vmin';
+        container.append(cell);
     }
 
-    //Create an array and fill the buttons
+    //Create an array and fill the cells
     arr = createArr(size);
     shuffledArr = shuffle([...arr]);
-    let buttons = Array.from(document.querySelectorAll('.wrongButton'));
-    buttons.forEach((button, index) => button.textContent = shuffledArr[index].toString());
+    let cells = Array.from(document.querySelectorAll('.cell'));
+    cells.forEach((cell, index) => cell.textContent = shuffledArr[index].toString());
 
-    buttons.sort((a, b) => +(a.textContent) - +(b.textContent));
-    buttons[0].classList.add('trueButton');
-    buttons.forEach((button, index) => button.addEventListener('click', function () {
-        let removeTrueButtonStyle = function () { button.classList.remove('trueButton'); };
-        if (+button.textContent === arr[0]) {
+    cells.sort((a, b) => +(a.textContent) - +(b.textContent));
+    cells[0].classList.add('trueCell');
+    cells.forEach((cell, index) => cell.addEventListener('click', function () {
+        if (+cell.textContent === arr[0]) {
             arr.shift();
-            if (buttons[index + 1]) {
-                buttons[index + 1].classList.add('trueButton');
-                setTimeout(removeTrueButtonStyle, 150);
+            if (cells[index + 1]) {
+                cells[index + 1].classList.add('trueCell');
+                setTimeout(() => cell.classList.remove('trueCell'), 150);  // trick for mobile platforms
             } else {
                 endTime = new Date().getTime();
-                setTimeout(removeTrueButtonStyle, 150);
+                setTimeout(() => cell.classList.remove('trueCell'), 150);
                 resultTime = ((endTime - startTime) / 1000).toFixed(1);
                 document.querySelector('.mainContainer').classList.add('hidden');
-                container.innerHTML = ""; //clear all buttons
+                container.innerHTML = ""; //clear all cells
                 document.querySelector('.startButton').textContent = "RESTART";              
                 result ? result.textContent = `${resultTime} sec` : makeResult();
-                document.querySelector('.initialContainer').classList.remove('hidden');
+                document.querySelector('.schulteContainer').classList.remove('hidden');
                 document.querySelector('.buttonContainer').prepend(result);
             }
         }
     }))
 }
+
+
+document.querySelector('.startButton').addEventListener('click', function () {
+    createTable();
+    document.querySelector('.mainContainer').classList.remove('hidden');
+    document.querySelector('.schulteContainer').classList.add('hidden');
+    startTime = new Date().getTime();
+})
